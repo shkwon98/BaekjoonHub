fba4ae03776e431b09dd9a818ac70f0d0484a743
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-17 16:04:08 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool checkGroupWord(string word)
+{
+    bool isGroupWord = true;
+    for(int i = 0; i < word.length(); ++i)
+    {
+        if(word[i] != word[i + 1])
+        {
+            for(int j = i + 2; j < word.length(); ++j)
+            {
+                if(word[i] == word[j])
+                {
+                    isGroupWord = false;
+                    break;
+                }
+            }
+        }
+    }
+    return isGroupWord;
+}
+
+int main()
+{
+    int nTotal;
+    string word;
+    cin >> nTotal;
+    
+    int nGroupWord = nTotal;
+    for(int i = 0; i < nTotal; ++i)
+    {
+        cin >> word;
+        if(!checkGroupWord(word)) nGroupWord--;
+    }
+
+    cout << nGroupWord;
+    return 0;
+}
