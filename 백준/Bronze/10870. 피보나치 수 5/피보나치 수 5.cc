937537524161629c1bfb2659616c5d9d08d2ff96
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-20 23:10:15 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -5,7 +5,7 @@ using namespace std;
 int fibonacci(int a)
 {
     if(a == 0) return 0;
-    if(a == 1 || a == 2) return 1;
+    if(a == 1) return 1;
     else return (fibonacci(a - 1) + fibonacci(a - 2));
 }
 
