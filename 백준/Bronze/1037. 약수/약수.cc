66cfd2a7736c1c023866807b5e822b1030b8daf4
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-22 01:54:09 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int arr[N];
+    
+    for(int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+    cout << arr[0] * arr[N - 1];
+    
+    return 0;
+}
