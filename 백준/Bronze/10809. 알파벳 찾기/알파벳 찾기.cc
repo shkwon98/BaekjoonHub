7460ec48e7d575d3cf012501a78e223fb6d77888
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-25 17:36:05 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -10,17 +10,13 @@ int main()
 {
     cin >> word;
     memset(alpha, -1, sizeof(alpha));
+    
     for(int i = 0; i < word.length(); ++i)
-    {
         for(int j = 0; j < 26; ++j)
-        {
             if(word[i] - 'a' == j && alpha[j] == -1) alpha[j] = i;
-        }
-    }
     
     for(int j = 0; j < 26; ++j)
-    {
         cout << alpha[j] << " ";
-    }
+    
     return 0;
 }
