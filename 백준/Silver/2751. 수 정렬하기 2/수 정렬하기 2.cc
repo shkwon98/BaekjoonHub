a3617353f169a7fe37fb0e783fe0cc74d67e2948
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-27 04:02:26 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 664 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -5,48 +5,16 @@ using namespace std;
 int N;
 int arr[10000001];
 
-void quickSort(int* data, int start, int end)
-{
-    if (start >= end) return;
-
-    int key = start;
-    int i = start + 1;
-    int j = end;
-    int temp;
-    while (i <= j)
-    {
-        while (i <= end && data[i] <= data[key])
-            i++;
-        while (j > start && data[j] >= data[key])
-            j--;
-        if (i > j)
-        {
-            temp = data[j];
-            data[j] = data[key];
-            data[key] = temp;
-        }
-        else
-        {
-            temp = data[j];
-            data[j] = data[i];
-            data[i] = temp;
-        }
-    }
-    quickSort(data, start, j - 1);
-    quickSort(data, j + 1, end);
-}
-
 int main()
 {
     cin >> N;
     for (int i = 0; i < N; ++i)
         cin >> arr[i];
 
-    random_shuffle(arr, arr + N);
-    quickSort(arr, 0, N - 1);
+    sort(arr, arr + N);
 
     for (int i = 0; i < N; ++i)
-    cout << arr[i] << "\n";
+        cout << arr[i] << "\n";
 
     return 0;
 }
