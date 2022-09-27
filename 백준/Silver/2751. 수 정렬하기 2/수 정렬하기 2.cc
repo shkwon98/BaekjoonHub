3c103b33686e94fe357fd14c63f7eef88b128085
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-27 16:03:09 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 644 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int N;
-int arr[10000001];
+int N, num;
+bool arr[2000001] = {};
 
 int main()
 {
     cin >> N;
     for (int i = 0; i < N; ++i)
-        cin >> arr[i];
+    {
+        cin >> num;
+        arr[num + 1000000] = true;
+    }
 
-    sort(arr, arr + N);
-
-    for (int i = 0; i < N; ++i)
-        printf("%d\n", arr[i]);
+    for (int i = 0; i <= 2000000; ++i)
+    {
+        if (arr[i])
+            cout << i - 1000000 << "\n";
+    }
 
     return 0;
 }
