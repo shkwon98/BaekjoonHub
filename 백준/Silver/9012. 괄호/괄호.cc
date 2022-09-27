533f2dc905b0df709f55d7228120a38621a07f71
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-28 02:04:24 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int N;
+vector<string> v;
+string input;
+
+void vps(string str)
+{
+    int strLen = str.length();
+    int cnt = 0;
+    for(int i = 0; i < strLen; ++i)
+    {
+        if(str[i] == '(') cnt++;
+        else cnt--;
+        
+        if(cnt < 0)
+        {
+            cout << "NO\n";
+            return;
+        }
+    }
+    if(cnt != 0)
+    {
+        cout << "NO\n";
+        return;
+    }
+    
+    cout << "YES\n";
+    return;
+}
+
+int main()
+{
+    cin >> N;
+    for(int i = 0; i < N; ++i)
+    {
+        cin >> input;
+        v.push_back(input);
+    }
+
+    for(int i = 0; i < N; ++i)
+    {
+        vps(v[i]);
+    }
+
+    return 0;
+}
