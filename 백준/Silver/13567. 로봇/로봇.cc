1f85159ff5ce1cf4f636091131a96e954325eda7
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-29 01:40:45 +0900

    [Silver IV] Title: 로봇, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/13567. 로봇/로봇.cc
+++ 백준/Silver/13567. 로봇/로봇.cc
@@ -5,23 +5,27 @@ using namespace std;
 
 int M, n;
 pair<int, int> map(0, 0);
+int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 bool isIn = true;
 
 void robot()
 {
     string cmd;
     int val;
-    int dir = 4000;
+    int idx = 0;
     for(int i = 0; i < n; ++i)
     {
         cin >> cmd >> val;
-        if(cmd == "MOVE" && dir % 4 == 0) map.first += val;
-        else if(cmd == "MOVE" && dir % 4 == 1) map.second += val;
-        else if(cmd == "MOVE" && dir % 4 == 2) map.first -= val;
-        else if(cmd == "MOVE" && dir % 4 == 3) map.second -= val;
-        else if(cmd == "TURN" && val == 0) dir++;
-        else if(cmd == "TURN" && val == 1) dir--;
-        
+        if(!isIn) continue;
+
+        if(cmd == "MOVE")
+        {
+            map.first += dir[idx][0] * val;
+            map.second += dir[idx][1] * val;
+        }
+        else if(cmd == "TURN" && val == 0) idx = (idx + 1) % 4;
+        else if(cmd == "TURN" && val == 1) idx = (idx + 3) % 4;
+
         if(map.first < 0 || map.first > M || map.second < 0 || map.second > M)
             isIn = false;
     }
@@ -31,9 +35,9 @@ void robot()
 int main()
 {
     cin >> M >> n;
-    
+
     robot();
-    
+
     if(isIn) cout << map.first << " " << map.second;
     else cout << -1;
 
