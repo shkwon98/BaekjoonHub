d85e5ade137e80fc994480a1326872c3ec10a60b
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-30 02:03:52 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int T, N;
+int arr[41] = {0, 1, };
+
+int fibonacci(int n)
+{
+    if(n == 0 || n == 1) return arr[n];
+    else if (arr[n] == 0) arr[n] = fibonacci(n - 2) + fibonacci(n - 1);
+    return arr[n];
+}
+int main()
+{
+    cin >> T;
+    for(int i = 0; i < T; ++i)
+    {
+        cin >> N;
+        if(N == 0)
+            cout << "1 0\n";
+        else
+            cout << fibonacci(N - 1) << " " << fibonacci(N) << "\n";
+    }
+
+    return 0;
+}
