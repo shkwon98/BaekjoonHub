0ffbb210e06c0b1b60ae05cca61a804260b0d50d
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-09-29 15:26:57 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 11428 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,58 +1,72 @@
 #include <iostream>
 #include <queue>
-#include <utility>
 using namespace std;
 
-int n, m, h;
-int tomato[102][102][102];
-int dz[6] = { 0,0,0 ,0,-1,1 };
-int dx[6] = {0,0 ,1,-1,0,0};
-int dy[6] = {-1,1,0,0,0,0 };
-
-int dist[102][102][102];
+#define MAX 100
+int M, N, H;
+int tomato[MAX][MAX][MAX];
+int dx[6] = {-1, 1, 0, 0, 0, 0};
+int dy[6] = {0, 0, -1, 1, 0, 0};
+int dz[6] = {0, 0, 0, 0, -1, 1};
+queue<pair<pair<int, int>, int> > q;
+int dist[MAX][MAX][MAX];
 int day = 0;
 
-int main() {
-	ios::sync_with_stdio(NULL);
+void bfs()
+{
+    while(!q.empty())
+    {
+        pair<pair<int, int>, int> cur = q.front();
+        q.pop();
+        for(int i = 0; i < 6; ++i)
+        {
+            int nx = cur.second + dx[i];
+            int ny = cur.first.second + dy[i];
+            int nz = cur.first.first + dz[i];
+            if(nx < 0 || nx >= M || ny < 0 || ny >= N || nz < 0 || nz >= H) continue;
+            if(dist[nz][ny][nx] >= 0) continue;
+            dist[nz][ny][nx] = dist[cur.first.first][cur.first.second][cur.second] + 1;
+            q.push({{nz, ny}, nx});
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
 	cin.tie(NULL);
-	cin >> m >> n >> h;
-	queue<pair<pair<int,int>,int>> Q;
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-			for(int k = 0; k < m ; k++) {
-				cin >> tomato[i][j][k];
-				if (tomato[i][j][k] == 1) {
-					Q.push({ {i,j},k });
-				}
-				if (tomato[i][j][k] == 0)
-					dist[i][j][k] = -1;
-			}
-		}
-	}
-	while (!Q.empty()) {
-		pair<pair<int, int>, int> cur = Q.front();
-		Q.pop();
-		for (int i = 0; i < 6; i++) {
-			int nz = cur.first.first + dz[i];
-			int nx = cur.first.second+ dx[i];
-			int ny = cur.second + dy[i];
-			
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m || nz < 0 || nz >= h) continue;
-			if (dist[nz][nx][ny] >= 0) continue;
-			dist[nz][nx][ny] = dist[cur.first.first][cur.first.second][cur.second] + 1;
-			Q.push({{ nz,nx },ny});
-		}
-	}
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < m; k++) {
-				if (dist[i][j][k] == -1) {
-					cout << -1;
-					return 0;
-				}
-				day = max(day, dist[i][j][k]);
-			}
-		}
-	}
-	cout << day;
+
+    cin >> M >> N >> H;
+    for(int i = 0; i < H; ++i)
+    {
+        for(int j = 0; j < N; ++j)
+        {
+            for(int k = 0; k < M; ++k)
+            {
+                cin >> tomato[i][j][k];
+                if(tomato[i][j][k] == 1) q.push({{i, j}, k});
+                if(tomato[i][j][k] == 0) dist[i][j][k] = -1;
+            }
+        }
+    }
+
+    bfs();
+
+    for(int i = 0; i < H; ++i)
+    {
+        for(int j = 0; j < N; ++j)
+        {
+            for(int k = 0; k < M; ++k)
+            {
+                if(dist[i][j][k] == -1)
+                {
+                    cout << -1;
+                    return 0;
+                }
+                day = max(day, dist[i][j][k]);
+            }
+        }
+    }
+    cout << day;
+    return 0;
 }
