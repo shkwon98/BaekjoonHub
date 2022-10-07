9139eb328ad9223c512eafaee9a94cc40fe59bd3
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-10-07 18:48:45 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,18 +1,10 @@
 #include <vector>
-#include <map>
+#include <set>
 #include <algorithm>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
-    map<int, int> poketmon;
-    for(int i = 0; i < nums.size(); ++i)
-    {
-        if(poketmon.find(nums[i]) == poketmon.end()) poketmon[nums[i]] = 1;
-        else poketmon[nums[i]]++;
-    }
-    answer = min(poketmon.size(), nums.size() / 2);
-    
-    return answer;
+    set<int> s(nums.begin(), nums.end());
+    return min(s.size(), nums.size() / 2);;
 }
