5cdc109e74feb07a6298d9623fd6ac8d627ec3ad
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-10-21 00:00:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -5,34 +5,11 @@ using namespace std;
 int solution(int n, int a, int b)
 {
     int answer = 0;
-    
-    int num = n;
-    while(num > 1)
+    while(a != b)
     {
-        num /= 2;
+        a = a/2 + a%2;
+        b = b/2 + b%2;
         answer++;
-    }
-    
-    int cp = n / 2;
-    int d = cp / 2;
-    while(answer > 1)
-    {
-        if(a <= cp && b <= cp)
-        {
-            cp -= d;
-            d /= 2;
-            answer--;
-            continue;
-        }
-        if(a > cp && b > cp)
-        {
-            cp += d;
-            d /= 2;
-            answer--;
-            continue;
-        }
-        
-        break;
     }
 
     return answer;
