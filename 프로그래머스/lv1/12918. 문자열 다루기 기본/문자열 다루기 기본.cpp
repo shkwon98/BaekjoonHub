0d9bfe61b07f6c2548902bdfd4376b5ff4c1c563
Seonghyeon Kwon <86837107+shkwon98@users.noreply.github.com>
2022-10-21 16:58:01 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    
+    if(s.length() != 4 && s.length() != 6) answer = false;
+    else
+    {
+        for(int i = 0; i < s.length(); ++i)
+        {
+            if(s[i] - '0' < 0 || s[i] - '9' > 0)
+            {
+                answer = false;
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
